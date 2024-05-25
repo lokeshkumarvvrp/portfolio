69bd5449958f8b8ef1b875f04fef289b865fe862
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-05-25 20:01:07 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x'); // Corrected class name to 'bx-x'
-    navbar.classList.toggle('open');
-};
-
-const sr = ScrollReveal ({
+const sr = ScrollReveal({
     distance: '40px',
     duration: 2500,
     reset: true
 });
 
-sr.reveal('.logo',{delay:200, origin: 'left'});
-sr.reveal('.navbar',{delay:400, origin: 'top'});
-sr.reveal('.menu-btn',{delay:520, origin: 'right'});
-sr.reveal('.home-text span',{delay:600, origin: 'top'});
-sr.reveal('.home-text h1',{delay:680, origin: 'left'});
-sr.reveal('.home-text p',{delay:750, origin: 'right'});
-sr.reveal('.main-btn',{delay:860, origin: 'left'});
-sr.reveal('.share',{delay:950, origin: 'bottom'});
-sr.reveal('.home-img',{delay:1000, origin: 'right'});
+// Reveal elements as the user scrolls
+sr.reveal('.logo', { delay: 200, origin: 'left' });
+sr.reveal('.navbar', { delay: 400, origin: 'top' });
+sr.reveal('.menu-btn', { delay: 520, origin: 'right' });
+sr.reveal('.home-text span', { delay: 600, origin: 'top' });
+sr.reveal('.home-text h1', { delay: 680, origin: 'left' });
+sr.reveal('.home-text p', { delay: 750, origin: 'right' });
+sr.reveal('.main-btn', { delay: 860, origin: 'left' });
+sr.reveal('.share', { delay: 950, origin: 'bottom' });
+sr.reveal('.home-img', { delay: 1000, origin: 'right' });
+sr.reveal('.skills', { delay: 1100, origin: 'bottom' });
+sr.reveal('.experiences', { delay: 1200, origin: 'bottom' });
+
+// Reveal applications section with a delay
+sr.reveal('.applications', { delay: 1300, origin: 'bottom' });
